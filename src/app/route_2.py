e2947e16f88d5e25022eb5b95daaeda365a88207
drevoborod <drevoborod@gmail.com>
2024-05-12 09:59:17 +0300

    First implementation of second algorithm.

--- src/app/route_2.py
+++ src/app/route_2.py
@@ -1,3 +1,5 @@
+import random
+
 from .base_route import BaseRoute
 from .field import Cell, PositionError
 
@@ -17,44 +19,81 @@ class Route2(BaseRoute):
         while True:
             if self.finish == current_cell:
                 return path
-            # saving current sell state to check later if it was changed:
-            previous_cell = current_cell
+
+            # potential new cells ranged by priority:
+            new_coords_prioritized: list[tuple[int, int]] = []
 
             # find out in which direction the closest path to the finish lays:
             x_diff = current_cell.x - self.finish.x
             y_diff = current_cell.y - self.finish.y
             if x_diff == 0:
                 if y_diff > 0:
-                    new_coords = (current_cell.x, current_cell.y - 1)
+                    new_coords_prioritized.append((current_cell.x, current_cell.y - 1))
+                    new_coords_prioritized.append((current_cell.x, current_cell.y + 1))
                 else:
-                    new_coords = (current_cell.x, current_cell.y + 1)
+                    new_coords_prioritized.append((current_cell.x, current_cell.y + 1))
+                    new_coords_prioritized.append((current_cell.x, current_cell.y - 1))
+                additional = [current_cell.x + 1, current_cell.x - 1]
+                random.shuffle(additional)  # let the fate decide :)
+                new_coords_prioritized.append((additional[0], current_cell.y))
+                new_coords_prioritized.append((additional[1], current_cell.y))
             elif y_diff == 0:
                 if x_diff > 0:
-                    new_coords = (current_cell.x - 1, current_cell.y)
+                    new_coords_prioritized.append((current_cell.x - 1, current_cell.y))
+                    new_coords_prioritized.append((current_cell.x + 1, current_cell.y))
                 else:
-                    new_coords = (current_cell.x + 1, current_cell.y)
+                    new_coords_prioritized.append((current_cell.x + 1, current_cell.y))
+                    new_coords_prioritized.append((current_cell.x - 1, current_cell.y))
+                additional = [current_cell.y + 1, current_cell.y - 1]
+                random.shuffle(additional)
+                new_coords_prioritized.append((current_cell.x, additional[0]))
+                new_coords_prioritized.append((current_cell.x, additional[1]))
             elif abs(x_diff) > abs(y_diff):
                 if x_diff > 0:
-                    new_coords = (current_cell.x - 1, current_cell.y)
+                    new_coords_prioritized.append((current_cell.x - 1, current_cell.y))
+                    additional = [(current_cell.x + 1, current_cell.y)]
                 else:
-                    new_coords = (current_cell.x + 1, current_cell.y)
+                    new_coords_prioritized.append((current_cell.x + 1, current_cell.y))
+                    additional = [(current_cell.x - 1, current_cell.y)]
+                if y_diff > 0:
+                    new_coords_prioritized.append((current_cell.x, current_cell.y - 1))
+                    additional.append((current_cell.x, current_cell.y + 1))
+                else:
+                    new_coords_prioritized.append((current_cell.x, current_cell.y + 1))
+                    additional.append((current_cell.x, current_cell.y - 1))
+                random.shuffle(additional)
+                new_coords_prioritized.append(additional[0])
+                new_coords_prioritized.append(additional[1])
             else:
                 if y_diff > 0:
-                    new_coords = (current_cell.x, current_cell.y - 1)
+                    new_coords_prioritized.append((current_cell.x, current_cell.y - 1))
+                    additional = [(current_cell.x, current_cell.y + 1)]
                 else:
-                    new_coords = (current_cell.x, current_cell.y + 1)
+                    new_coords_prioritized.append((current_cell.x, current_cell.y + 1))
+                    additional = [(current_cell.x, current_cell.y - 1)]
+                if x_diff > 0:
+                    new_coords_prioritized.append((current_cell.x - 1, current_cell.y))
+                    additional.append((current_cell.x + 1, current_cell.y + 1))
+                else:
+                    new_coords_prioritized.append((current_cell.x + 1, current_cell.y))
+                    additional.append((current_cell.x - 1, current_cell.y))
+                random.shuffle(additional)
+                new_coords_prioritized.append(additional[0])
+                new_coords_prioritized.append(additional[1])
 
-            if self._cell_can_be_used(new_coords, path, blacklist):
-                if path[-1] != current_cell:    # to avoid duplicates when returning to previously visited cell
-                    path.append(current_cell)
-                current_cell = self.get_cell(*new_coords)
+            for new_coords in new_coords_prioritized:
+                if self._cell_can_be_used(new_coords, path, blacklist):
+                    if path[-1] != current_cell:    # to avoid duplicates when returning to previously visited cell
+                        path.append(current_cell)
+                    current_cell = self.get_cell(*new_coords)
+                    break
+                else:
+                    try:
+                        blacklist.add(self.get_cell(*new_coords))
+                    except PositionError:
+                        pass
             else:
-                try:
-                    blacklist.add(self.get_cell(*new_coords))
-                except PositionError:
-                    pass
-            # if we made no step forwards, add current and previous cells to blacklist and make step backwards:
-            if previous_cell is current_cell:
+                # if we made no step forwards, add current and previous cells to blacklist and make step backwards:
                 blacklist.add(current_cell)
                 blacklist.add(path.pop())
                 current_cell = path[-1]
--- src/check_route_2.py
+++ src/check_route_2.py
@@ -7,14 +7,14 @@ from app.route_2 import Route2
 
 if __name__ == "__main__":
     ## Creating game field:
-    f = Field().build(20, 20)
+    f = Field().build(100, 100)
 
     ## Placing obstacles on the field:
-    # f.set_cell_state((3, 4), (5, 4), (3, 3), (5, 3), CellCondition.blocked)
-    # for x in range(3, 6):
-    #     f.set_cell_state(x, 5, CellCondition.blocked)
-    # for y in range(7, 18):
-    #     f.set_cell_state(8, y, CellCondition.blocked)
+    f.set_cell_state((3, 4), (5, 4), (3, 3), (5, 3), CellCondition.blocked)
+    for x in range(3, 6):
+        f.set_cell_state(x, 5, CellCondition.blocked)
+    for y in range(7, 18):
+        f.set_cell_state(8, y, CellCondition.blocked)
 
     # Increase complexity:
     # for x in range(8, 21):
@@ -29,14 +29,14 @@ if __name__ == "__main__":
     #     f.set_cell_state(x, 6, CellCondition.blocked)
 
     # Increase complexity 4:
-    # for x in range(8, 21):
-    #     f.set_cell_state(x, 6, CellCondition.blocked)
-    # for x in range(2, 6):
-    #     f.set_cell_state(x, 12, CellCondition.blocked)
-    # for y in range(7, 12):
-    #     f.set_cell_state(4, y, CellCondition.blocked)
-    # for y in range(1, 5):
-    #     f.set_cell_state(13, y, CellCondition.blocked)
+    for x in range(8, 20):
+        f.set_cell_state(x, 6, CellCondition.blocked)
+    for x in range(2, 6):
+        f.set_cell_state(x, 12, CellCondition.blocked)
+    for y in range(7, 12):
+        f.set_cell_state(4, y, CellCondition.blocked)
+    for y in range(1, 5):
+        f.set_cell_state(13, y, CellCondition.blocked)
 
     ## Drawing just a field with obstacles:
     # draw_field(f)
@@ -49,12 +49,13 @@ if __name__ == "__main__":
     # r = Route2(f, f(4, 4), f(6, 12))
     # Reversed route:
     # r = Route2(f, f(16, 12), f(4, 4))
+    r = Route2(f, f(16, 12), f(3, 11))
     # Reversed route 2:
     # r = Route2(f, f(6, 12), f(4, 4))
     # Reversed x route 1:
     # r = Route2(f, f(16, 4), f(4, 14))
     # Reversed y route 1:
-    r = Route2(f, f(4, 14), f(14, 4))
+    # r = Route2(f, f(4, 14), f(14, 4))
 
     ## Drawing a field with route on it:
     draw_route(r)
